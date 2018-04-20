c40b8259b1845c6fc6ac4bef7b1ed1dbb2cf5648
Luca Gallici <lg@administrates.co>
2018-04-20 15:50:11 +0100

    Added integration with CoreApp

--- ui/jobs.py
+++ ui/jobs.py
@@ -1,8 +1,15 @@
-from flask import render_template
+import requests
+from flask import render_template, request
 
 from lib.auth import login_required
 
 
 @login_required
 def list_jobs():
-    return render_template("jobs.jinja2")
+
+    jwt = request.cookies.get('jwt')
+    url = 'http://localhost:5000'+'/api/job'
+    jobs = requests.get(url,
+                        headers={'Authorization': 'Bearer ' + jwt})
+
+    return render_template("jobs.jinja2", jobs=jobs.json()['results'])
