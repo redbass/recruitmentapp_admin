755d8c03595a9faefaa7ad22f6ae09c63e42e4bc
Luca Gallici <lg@administrates.co>
2018-07-15 12:05:18 +0100

    Added job edit

--- ui/job.py
+++ ui/job.py
@@ -29,6 +29,42 @@ def create_job_post():
         }
     }
 
-    response = make_admin_core_api_call('/admin/api/job', data=data)
+    make_admin_core_api_call('/admin/api/job', data=data)
+
+    return redirect(url_for('jobs'))
+
+
+@login_required
+def edit_job_view(job_id):
+    job = make_admin_core_api_call('/api/job/' + job_id).json()
+
+    companies = make_admin_core_api_call('/api/company').json()
+    company = next(company for company in companies
+                   if company['_id'] == job['company_id'])
+
+    initial_market_latlng = {
+        'lat': job['location']['coordinates'][1],
+        'lng': job['location']['coordinates'][0]
+    }
+
+    return render_template("job/edit_job.jinja2",
+                           job=job,
+                           initial_market_latlng=initial_market_latlng,
+                           company=company)
+
+
+@login_required
+def edit_job_post(job_id):
+
+    data = {
+        "title": request.form.get('title'),
+        "description": request.form.get('description'),
+        "location": {
+            "lat": request.form.get('latitude'),
+            "lng": request.form.get('longitude')
+        }
+    }
+
+    make_admin_core_api_call('/api/job/' + job_id, data=data)
 
     return redirect(url_for('jobs'))
