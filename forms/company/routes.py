1f9e19e6cf5dbc544e831951039f4e2a96838653
Luca Gallici <lg@administrates.co>
2018-08-11 15:04:44 +0200

    moved edit_company to use wtf

---
+++ forms/company/routes.py
@@ -0,0 +1,59 @@
+from flask import request, render_template, redirect, url_for, flash
+
+from forms.company.form import CompanyForm
+from lib.auth import login_required
+from lib.core_integration import post_json_to_core, APICallError, get_json_from_core
+from lib.exceptions import AuthenticationError
+
+
+@login_required
+def create_company_view():
+    form = CompanyForm(request.form)
+    return render_template("company/create_company.jinja2", form=form)
+
+
+@login_required
+def create_company_post():
+    form = CompanyForm(request.form)
+
+    if form.validate_on_submit():
+
+        try:
+            data = form.create_company_core_from_form()
+            response = post_json_to_core('/api/company', json=data)
+            return redirect(url_for('companies'))
+
+        except [APICallError, AuthenticationError] as e:
+            flash(str(e))
+
+    return render_template("company/create_company.jinja2", form=form)
+
+
+@login_required
+def edit_company_view(company_id):
+    company = get_json_from_core('/api/company/' + company_id)
+
+    form = CompanyForm(request.form)
+
+    form.populate_form_from_core(company)
+
+    return render_template("company/edit_company.jinja2", form=form,
+                           company_id=company.get('_id'))
+
+
+@login_required
+def edit_company_post(company_id):
+
+    form = CompanyForm(request.form)
+
+    if form.validate_on_submit():
+
+        try:
+            data = form.create_company_core_from_form()
+            response = post_json_to_core('/api/company/' + company_id, json=data)
+            return redirect(url_for('companies'))
+
+        except [APICallError, AuthenticationError] as e:
+            flash(str(e))
+
+    return render_template("company/edit_company.jinja2", form=form)
