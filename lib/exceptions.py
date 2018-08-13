bff1eca9a5f27b7aa96c33c60c7b1a26ab6edcbe
Luca Gallici <lg@administrates.co>
2018-08-13 16:16:04 +0200

    Refactoring of create and edit company

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -11,3 +11,14 @@ class AuthenticationError(BaseRecruitmentAppException):
 
     def __init__(self, msg: str = None, ref_id: str = None):
         super().__init__(msg or 'Authentication error', ref_id)
+
+
+class APICallError(BaseRecruitmentAppException):
+    pass
+
+
+class APIValidationError(BaseRecruitmentAppException):
+
+    def __init__(self, core_response_body):
+        super().__init__(msg=core_response_body.get('message'),
+                         ref_id=core_response_body.get('ref_id'))
