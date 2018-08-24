f9fd5ddb5965bc32d66d14a1d0f3522d76d607e7
Luca Gallici <lg@administrates.co>
2018-08-24 19:55:45 +0100

    Edit job now use WTF

--- ui/job.py
+++ ui/job.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, url_for, redirect
+from flask import render_template
 
 from lib.auth import login_required
-from lib.core_integration import get_json_from_core, post_json_to_core
+from lib.core_integration import get_json_from_core
 
 
 @login_required
@@ -11,37 +11,3 @@ def jobs_view():
     return render_template("job/jobs.jinja2", jobs=jobs)
 
 
-@login_required
-def edit_job_view(job_id):
-    job = get_json_from_core('/api/job/' + job_id)
-    companies = get_json_from_core('/api/company')
-
-    company = next(company for company in companies
-                   if company['_id'] == job['company_id'])
-
-    initial_market_latlng = {
-        'lat': job['location']['coordinates'][1],
-        'lng': job['location']['coordinates'][0]
-    }
-
-    return render_template("job/edit_job.jinja2",
-                           job=job,
-                           initial_market_latlng=initial_market_latlng,
-                           company=company)
-
-
-@login_required
-def edit_job_post(job_id):
-
-    data = {
-        "title": request.form.get('title'),
-        "description": request.form.get('description'),
-        "location": {
-            "lat": request.form.get('latitude'),
-            "lng": request.form.get('longitude')
-        }
-    }
-
-    post_json_to_core('/api/job/' + job_id, json=data)
-
-    return redirect(url_for('jobs'))
