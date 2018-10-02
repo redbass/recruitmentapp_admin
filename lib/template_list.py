6fff0a3c7c8b2a9b29c904990e380ebf86694c55
Luca Gallici <lg@administrates.co>
2018-10-02 22:59:35 +0100

    Created common crate_job template

--- lib/template_list.py
+++ lib/template_list.py
@@ -9,11 +9,12 @@ ADMIN_CREATE_COMPANY = "admin/create_company.jinja2"
 ADMIN_EDIT_COMPANY = "admin/edit_company.jinja2"
 ADMIN_COMPANY_LIST = "admin/companies.jinja2"
 ADMIN_JOB_LIST = 'admin/jobs.jinja2'
-ADMIN_CREATE_JOB = "admin/create_job.jinja2"
 ADMIN_EDIT_JOB = "admin/edit_job.jinja2"
+
+# COMMON
+COMMON_CREATE_JOB = "common/create_job.jinja2"
 
 # HIRING MANAGER
 HM_EDIT_COMPANY = "hiring_manager/edit_company.jinja2"
 HM_JOB_LIST = 'hiring_manager/jobs.jinja2'
-HM_CREATE_JOB = "hiring_manager/create_job.jinja2"
 HM_EDIT_JOB = "hiring_manager/edit_job.jinja2"
