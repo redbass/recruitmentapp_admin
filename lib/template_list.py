ed061900012e208d21734972983c8474b62b98f2
Luca Gallici <lg@administrates.co>
2018-10-02 23:21:17 +0100

    Created common template for jobs list

--- lib/template_list.py
+++ lib/template_list.py
@@ -7,12 +7,11 @@ ERROR = "errpr.jinja"
 SIGN_IN_COMPANY = "admin/sign_in_company.jinja2"
 ADMIN_CREATE_COMPANY = "admin/create_company.jinja2"
 ADMIN_COMPANY_LIST = "admin/companies.jinja2"
-ADMIN_JOB_LIST = 'admin/jobs.jinja2'
 
 # COMMON
 COMMON_EDIT_COMPANY = "common/edit_company.jinja2"
 COMMON_CREATE_JOB = "common/create_job.jinja2"
 COMMON_EDIT_JOB = "common/edit_job.jinja2"
+COMMON_JOB_LIST = 'common/jobs.jinja2'
 
 # HIRING MANAGER
-HM_JOB_LIST = 'hiring_manager/jobs.jinja2'
