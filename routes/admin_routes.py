58a17341511a3754acb9767bf0176fa11e94f64d
Luca Gallici <lg@administrates.co>
2018-11-10 14:30:41 +0000

    Added users list

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -2,6 +2,9 @@ from routes.admin.company_routes import create_company_view, \
     create_company_post, edit_company_view, edit_company_post, companies_view
 from routes.admin.job_routes import create_job_post, \
     edit_job_view, edit_job_post, jobs_view, create_job_view
+from routes.admin.user import users_view
+
+ADMIN_USERS = '/admin/users'
 
 ADMIN_JOBS = '/admin/jobs'
 ADMIN_JOBS_ADD = '/admin/jobs/add'
@@ -17,6 +20,12 @@ ADMIN_SIGN_IN_COMPANY = '/admin/companies/sign_in'
 def add_admin_routes(app):
     _add_job_routes(app)
     _add_company_routes(app)
+    _add_user_routes(app)
+
+
+def _add_user_routes(app):
+    app.add_url_rule(ADMIN_USERS, 'admin_users',
+                     users_view, methods=['GET'])
 
 
 def _add_job_routes(app):
