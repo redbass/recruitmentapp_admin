e25980f3c717ac623e0450d83039e92f0a077025
Luca Gallici <lg@administrates.co>
2018-11-10 15:16:53 +0000

    Added table of users

--- routes/admin/user.py
+++ routes/admin/user.py
@@ -2,9 +2,12 @@ from flask import render_template
 
 from lib import template_list
 from lib.auth import login_required, ADMIN_ROLE
+from lib.core_integration import get_json_from_core
 
 
 @login_required(ADMIN_ROLE)
 def users_view():
 
-    return render_template(template_list.ADMIN_USERS)
+    users = get_json_from_core('/api/user')
+
+    return render_template(template_list.ADMIN_USERS, users=users)
