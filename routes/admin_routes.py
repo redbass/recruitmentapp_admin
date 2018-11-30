289fb8db19971239719c29babb61dc3eb2911224
Luca Gallici <lg@administrates.co>
2018-11-30 18:30:15 +0000

    Add company enable disable

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,5 +1,6 @@
 from routes.admin.company_routes import create_company_view, \
-    create_company_post, edit_company_view, edit_company_post, companies_view
+    create_company_post, edit_company_view, edit_company_post, \
+    companies_view, enable_company_post
 from routes.admin.job_routes import create_job_post, \
     edit_job_view, edit_job_post, jobs_view, create_job_view
 from routes.admin.settings import settings_view, upload_picklist, \
@@ -20,6 +21,7 @@ ADMIN_JOB = '/admin/jobs/<job_id>'
 ADMIN_COMPANIES = '/admin/companies'
 ADMIN_ADD_COMPANY = '/admin/companies/add'
 ADMIN_COMPANY = '/admin/companies/<company_id>'
+ADMIN_COMPANY_ACTIONS = '/admin/companies/<company_id>/<action>'
 
 ADMIN_SIGN_IN_COMPANY = '/admin/companies/sign_in'
 
@@ -76,3 +78,5 @@ def _add_company_routes(app):
                      edit_company_view, methods=['GET'])
     app.add_url_rule(ADMIN_COMPANY, 'edit_company_post',
                      edit_company_post, methods=['POST'])
+    app.add_url_rule(ADMIN_COMPANY_ACTIONS, 'company_actions_post',
+                     enable_company_post, methods=['POST'])
