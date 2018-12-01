db751ed377c742305d61cf6270931fc1a483de4b
Luca Gallici <lg@administrates.co>
2018-12-01 19:56:26 +0000

    Get logo return empty image if logo not found

--- routes/common/company.py
+++ routes/common/company.py
@@ -1,8 +1,12 @@
-from flask import request, jsonify
+from io import BytesIO
 
+from flask import request, jsonify, send_file
+
+from config import APP_SRC
 from lib.auth import login_required
 from lib.core_integration import post_json_to_core, get_from_core, post_to_core
 from lib.errors import flash_exception
+from lib.exceptions import APICallError
 
 
 def edit_company(form, company_id):
@@ -22,9 +26,18 @@ def edit_company(form, company_id):
 
 @login_required()
 def get_company_logo(company_id):
-    return get_from_core(
-        is_admin=False,
-        path='/api/company/{company_id}/logo'.format(company_id=company_id))
+    try:
+        url = '/api/company/{company_id}/logo'.format(company_id=company_id)
+        image = get_from_core(is_admin=False, path=url)
+        return send_file(
+                BytesIO(image),
+                mimetype='image/*'
+            )
+    except APICallError:
+        return send_file(
+            "{src}/img/empty.png".format(src=APP_SRC),
+            mimetype='image/png'
+        )
 
 
 @login_required()
